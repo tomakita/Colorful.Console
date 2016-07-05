5aa8aa6fc5a41b622be7d577de3766dc277f81b1
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-07-05 17:20:45 +0100

    Refactor

--- src/Colorful.Console/ColorExtensions.cs
+++ src/Colorful.Console/ColorExtensions.cs
@@ -20,8 +20,14 @@
             foreach (ConsoleColor consoleColor in Enum.GetValues(typeof(ConsoleColor)))
             {
                 string consoleColorName = Enum.GetName(typeof(ConsoleColor), consoleColor);
-                Color rgbColor = System.Drawing.Color.FromName(consoleColorName == "DarkYellow" ? "Orange" : consoleColorName);
-                double sum = Math.Pow(rgbColor.R - color.R, 2.0) + Math.Pow(rgbColor.G - color.G, 2.0) + Math.Pow(rgbColor.B - color.B, 2.0);
+                consoleColorName = string.Equals(consoleColorName, nameof(ConsoleColor.DarkYellow), StringComparison.Ordinal) ?
+                    nameof(Color.Orange) :
+                    consoleColorName;
+                Color rgbColor = Color.FromName(consoleColorName);
+                double sum =
+                    Math.Pow(rgbColor.R - color.R, 2.0) +
+                    Math.Pow(rgbColor.G - color.G, 2.0) +
+                    Math.Pow(rgbColor.B - color.B, 2.0);
 
                 if (sum == 0.0)
                 {
