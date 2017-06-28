c7cd4d997996b10608f2eaa85c2c8db8255acf52
Tom <takita@wesleyan.edu>
2017-06-27 21:20:47 -0500

    Removed nonfunctional ColorMapper test

--- src/Colorful.Console.Tests/ColorMapperTests.cs
+++ src/Colorful.Console.Tests/ColorMapperTests.cs
@@ -4,13 +4,6 @@ namespace Colorful.Console.Tests
 {
     public sealed class ColorMapperTests
     {
-        // TODO: This test doesn't run, because it requires CoreCompat.System.Drawing, which can't coexist with Xunit, yet.
-        [Fact]
-        public void MapColor_ThrowsException_WhenCalledAndConsoleWindowIsntOpen()
-        {
-            ColorMapper mapper = new ColorMapper();
-
-            Assert.Throws<ColorMappingException>(() => mapper.MapColor(ColorStoreTests.TEST_CONSOLE_COLOR, ColorStoreTests.TEST_COLOR));
-        }
+        // TODO
     }
 }
