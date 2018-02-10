0571de0391169e218b78495911f3103fc28e96b1
Tom Akita <tomakita@gmail.com>
2018-02-09 18:27:21 -0600

    Updated copyright year

--- src/Colorful.Console/Properties/AssemblyInfo.cs
+++ src/Colorful.Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Colorful.Console")]
-[assembly: AssemblyCopyright("Copyright © Tom Akita 2017")]
+[assembly: AssemblyCopyright("Copyright © Tom Akita 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
